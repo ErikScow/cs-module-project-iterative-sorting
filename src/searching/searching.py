b1d02a5579d2b09ec9b55c4107e8332f504f890e
erikscow <erikscow@gmail.com>
2020-09-30 20:11:29 -0700

    completed search algorithms

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,8 +7,19 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-    # Your code here
+    while low <= high:
+        middle = (low + high)//2
+        if target == arr[middle]:
+            return middle
+        if target < arr[middle]:
+            high = middle - 1
+        else:
+            low = middle + 1
+        
+
 
 
     return -1  # not found
